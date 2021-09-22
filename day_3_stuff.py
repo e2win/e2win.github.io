112ec0ed20113b16af7f61fd8c68179c5ea396f1
e2win <87226096+e2win@users.noreply.github.com>
2021-09-22 17:44:04 -0600

    Work of day 3

---
+++ day_3_stuff.py
@@ -0,0 +1,13 @@
+# let's see stuff work
+
+# This is an infinite loop
+# while(1==1):
+#    print("i")
+
+#strings are immutable
+a = "we went to the store"
+
+new_string = str("W") + a
+print(new_string)
+new_string = a[0] + 'W' + a[4]
+print(new_string)
